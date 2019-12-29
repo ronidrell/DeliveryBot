445eb2cf8f02c03fabcbf4c32e337e2508613437
ronidrell <54766900+ronidrell@users.noreply.github.com>
2019-12-29 17:07:31 +0900

    Create menu_config.py

---
+++ menu_config.py
@@ -0,0 +1,23 @@
+MENU = {
+    "salaty": {
+        "salat1": {
+            "desc": "blabla",
+            "price": 140
+        },
+        "salat2": {
+            "desc": "blablalal",
+            "price": 120
+        }
+    },
+    "osnovnoe": {
+        "sup": {
+            "desc": "bla bla",
+            "price": 50
+        },
+        "vtoroe": {
+            "desc": "bla bla",
+            "price": 100
+        }
+    }
+
+}
